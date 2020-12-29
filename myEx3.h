115a1948dafaad2cce66affc818e9a028a6a1a88
Tehila <tehilabk123@gmail.com>
2020-12-29 20:55:16 +0200

    initial commit

---
+++ myEx3.h
@@ -0,0 +1,14 @@
+#ifndef MYEX3_H
+#define MYEX3_H
+
+void shift_element(int* arr, int i);
+void insertion_sort(int* arr , int len);
+int check(int *arr, int i);
+void printArr(int* arr , int size);
+
+
+int getLine(char arr[], char word[]);
+void getword(char arr[], char word[]);
+int containWord(char arr[], char word[]);
+
+#endif //MYEX3_H
